227034681e75d7b04595a0b38824684b28fe7d00
zhouzhou <zhouzhou@mail.51vv.com>
2020-08-25 15:38:40 +0800

    添加

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@
  * Email: zhousaito@163.com
  * Created by zhousaito 2020/8/19 10:53
  * Version: 1.0
- * Description:@@@
+ * Description:777
  */
 public class Test {
     public static void main(String[] args) {
@@ -10,11 +10,11 @@ public class Test {
         //21474836472
         int value = Integer.MAX_VALUE + 2;
         //测修改
-        System.out.println("value@@ = " + value);
+        System.out.println("value@@&& = " + value);
 //        int value2 = ;
         System.out.println("" + (Integer.MIN_VALUE + 1));
         
-        String str = "你@@@@@%%^^&&";
+        String str = "你@%%^^&&***";
         str.indexOf();
     }
 }
