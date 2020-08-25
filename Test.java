786f597b55b17f28c307602688ab067cd5af8aa1
zhouzhou <zhouzhou@mail.51vv.com>
2020-08-25 10:48:52 +0800

    修改Test.java

--- Test.java
+++ Test.java
@@ -2,32 +2,18 @@
  * Email: zhousaito@163.com
  * Created by zhousaito 2020/8/19 10:53
  * Version: 1.0
-<<<<<<< HEAD
- * Description: lue
- */
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("hello world!!");
-=======
  * Description:
  */
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
->>>>>>> master
         //21474836472
         int value = Integer.MAX_VALUE + 2;
         System.out.println("" + value);
 //        int value2 = ;
         System.out.println("" + (Integer.MIN_VALUE + 1));
-<<<<<<< HEAD
         
         String str = "你好";
         str.indexOf();
-=======
-
-        String s = "";
-        s.indexOf()
->>>>>>> master
     }
 }
