b3d42f00365a12a3c443ef51b97e6524fcc92e6b
zhouzhou <zhouzhou@mail.51vv.com>
2020-08-25 11:24:41 +0800

    修改修改two

--- Test.java
+++ Test.java
@@ -2,19 +2,19 @@
  * Email: zhousaito@163.com
  * Created by zhousaito 2020/8/19 10:53
  * Version: 1.0
- * Description:lam
+ * Description:@@@
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello world!!!");
+        System.out.println("hello worldadafda!!!");
         //21474836472
         int value = Integer.MAX_VALUE + 2;
         //测修改
-        System.out.println("value = " + value);
+        System.out.println("value@@ = " + value);
 //        int value2 = ;
         System.out.println("" + (Integer.MIN_VALUE + 1));
         
-        String str = "你@@";
+        String str = "你@@@@@";
         str.indexOf();
     }
 }
