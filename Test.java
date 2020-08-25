6ccabac198d449a9ca3bd7577aea6e6d02612920
zhouzhou <zhouzhou@mail.51vv.com>
2020-08-25 15:38:40 +0800

    添加uag

--- Test.java
+++ Test.java
@@ -6,15 +6,23 @@
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello worldadafda!!!");
+        System.out.println("hello worl!!!adfaf");
         //21474836472
         int value = Integer.MAX_VALUE + 2;
         //测修改
+<<<<<<< HEAD
         System.out.println("value@@&& = " + value);
 //        int value2 = ;
         System.out.println("" + (Integer.MIN_VALUE + 1));
         
         String str = "你@%%^^&&***";
+=======
+        System.out.println("value@@&&^^ = " + value);
+//        int value2 = ;
+        System.out.println("" + (Integer.MIN_VALUE + 1));
+        
+        String str = "你@%%999999";
+>>>>>>> 添加uag
         str.indexOf();
     }
 }
