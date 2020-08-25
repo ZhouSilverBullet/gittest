03c024af6c79db248a78ae47256b15cf1c74447e
zhouzhou <zhouzhou@mail.51vv.com>
2020-08-25 16:03:28 +0800

    你好啊->修改了代码->stash取出提交->再次修改

--- Test.java
+++ Test.java
@@ -1,18 +1,28 @@
 /**
- * Email: zhousaito@163.com
- * Created by zhousaito 2020/8/19 10:53
- * Version: 1.0
- * Descript:aaa777
+ * E to@163.com我来进行先修改
+ * Created bydd zhousaito 2020/8/9 10:53
+ * Version: 1.0.iadfa
+ * Descript:aaa777sfasfasfas
+ * adsfa;dfa
+ * adfadfsa
+ * adfadf
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello worl!!!777");
+        System.out.println("@@@h worl!!!777");
         //21474836472
+        //asfasdf
         int value = Integer.MAX_VALUE + 2;
         //测修改
+        //asfasf
         System.out.println("" + (Integer.MIN_VALUE + 1));
-        
-        String str = "你@%%99999i9===--";
-        str.indexOf();
+        //afdasfasd
+        String s = 
+            safasdfas =  
+            "你@%%99999i9===--";
+        s.indexOf();
+        s.dddadafadf()asdfa;adfafd;asfa
+            //todo;
+            //asfasfda
     }
 }
