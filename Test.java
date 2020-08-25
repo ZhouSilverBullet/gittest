20157428bc87bb5288aa5d936cf7e8785b58a0a7
zhouzhou <zhouzhou@mail.51vv.com>
2020-08-25 15:52:28 +0800

    修改eee

--- Test.java
+++ Test.java
@@ -2,27 +2,17 @@
  * Email: zhousaito@163.com
  * Created by zhousaito 2020/8/19 10:53
  * Version: 1.0
- * Description:777
+ * Descript:aaa777
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello worl!!!adfaf");
+        System.out.println("hello worl!!!777");
         //21474836472
         int value = Integer.MAX_VALUE + 2;
         //测修改
-<<<<<<< HEAD
-        System.out.println("value@@&& = " + value);
-//        int value2 = ;
         System.out.println("" + (Integer.MIN_VALUE + 1));
         
-        String str = "你@%%^^&&***";
-=======
-        System.out.println("value@@&&^^ = " + value);
-//        int value2 = ;
-        System.out.println("" + (Integer.MIN_VALUE + 1));
-        
-        String str = "你@%%999999";
->>>>>>> 添加uag
+        String str = "你@%%99999i9===--";
         str.indexOf();
     }
 }
